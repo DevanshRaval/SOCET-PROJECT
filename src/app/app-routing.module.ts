78783580d2c8cb700f5dafacd456ed3692c7825b
Devansh Raval <devansh.r@grubbrr.com>
2023-04-14 15:01:48 +0530

    Home page header and nav created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { HomeComponent } from './Components/home/home.component';
+import { MeComponent } from './Components/me/me.component';
+import { MyTeamComponent } from './Components/my-team/my-team.component';
+import { OrgComponent } from './Components/org/org.component';
+import { MyHomeComponent } from './Components/my-home/my-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'login', component : LoginComponent},
+  {path : 'home', component : HomeComponent},
+  {path : 'me', component : MeComponent},
+  {path : 'my-team', component : MyTeamComponent},
+  {path : 'org', component : OrgComponent},
+  {path : 'my-home', component : MyHomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './Components/login/login.component';
 import { MeComponent } from './Components/me/me.component';
 import { HomeComponent } from './Components/home/home.component';
+import { MyTeamComponent } from './Components/my-team/my-team.component';
+import { OrgComponent } from './Components/org/org.component';
+import { MyHomeComponent } from './Components/my-home/my-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     MeComponent,
-    HomeComponent
+    HomeComponent,
+    MyTeamComponent,
+    OrgComponent,
+    MyHomeComponent
   ],
   imports: [
     BrowserModule,
