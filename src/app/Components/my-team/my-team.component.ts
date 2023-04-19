2488a5210edae15f12aba81ed4edecf396b75a10
Devansh Raval <devansh.r@grubbrr.com>
2023-04-19 15:52:05 +0530

    peers added + minor css

--- src/app/Components/my-team/my-team.component.ts
+++ src/app/Components/my-team/my-team.component.ts
@@ -12,4 +12,35 @@ export class MyTeamComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  empArray = [
+    {name : 'Devansh Raval', location : 'aaaa', department : 'Ahmedabad' , email : 'a1@gmail.com' , mobile : '9879717455'},
+    {name : 'Dhruvit Shah', location : 'bbbb', department : 'Ahmedabad' , email : 'b2@gmail.com' , mobile : '9879717455'},
+    {name : 'Vanraj Ghed', location : 'cccc', department : 'Ahmedabad' , email : 'c3@gmail.com' , mobile : '9879717455'},
+    {name : 'Vasantkumar  Prajapati', location : 'dddd', department : 'Ahmedabad' , email : 'd4@gmail.com' , mobile : '9879717455'},
+    {name : 'Vinod Pawar', location : 'eeee', department : 'Ahmedabad' , email : 'e5@gmail.com' , mobile : '9879717455'},
+    {name : 'Yogesh Gadhavi', location : 'ffff', department : 'Ahmedabad' , email : 'f6@gmail.com' , mobile : '9879717455'},
+    {name : 'Shrinath Shetty', location : 'gggg', department : 'Ahmedabad' , email : 'g7@gmail.com' , mobile : '9879717455'},
+    {name : 'Sonu Sharma', location : 'hhhh', department : 'Ahmedabad' , email : 'h8@gmail.com' , mobile : '9879717455'},
+    {name : 'Sagar Sahani', location : 'iiii', department : 'Ahmedabad' , email : 'i9@gmail.com' , mobile : '9879717455'},
+  ]
+
+  monthNumber = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
+
+  firstLetter(name : any){
+    let result = ' ';
+    let v = true;
+    for(let i = 0 ; i< name.length ; i++){
+      if(name[i]==' '){
+        v= true;
+      }
+
+      else if( name[i]!==' ' && v === true){
+        result += name[i];
+        v = false;
+      }
+      
+    }
+     return result;
+  }
+
 }
